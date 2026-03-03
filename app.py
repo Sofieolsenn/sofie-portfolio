9f9f33009292b17ede1f1e5e9e8053cb28648871
Sofieolsenn <165768920+Sofieolsenn@users.noreply.github.com>
2026-03-03 19:10:32 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
+from flask_session import Session
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+import x 
+import time
+import uuid
+import os
+import requests
+import json
+import csv
+import io
+
+from icecream import ic
+ic.configureOutput(prefix=f'----- | ', includeContext=True)
+
+app = Flask(__name__)
+
+# Set the maximum file size to 10 MB
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024   # 1 MB
+
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+ 
+
+##############################
+##############################
+##############################
+def _____USER_____(): pass 
+##############################
+##############################
+##############################
+
+##############################
+@app.context_processor
+def global_variables():
+    return dict (
+        x = x
+    )
+
+############################## 
+@app.get("/")
+def view_index():
+        return render_template("index.html")
+  
+
+##############################
+@app.get("/get-data-from-sheet")
+def get_data_from_sheet():
+    try:
+        url= f"https://docs.google.com/spreadsheets/d/{x.google_spread_sheet_key}/export?format=csv&id={x.google_spread_sheet_key}"
+        res=requests.get(url=url)
+        # ic(res.text) # contains the csv text structure
+        csv_text = res.content.decode('utf-8')
+        csv_file = io.StringIO(csv_text) # Use StringIO to treat the string as a file
+       
+        # Initialize an empty list to store the data
+        data = {}
+ 
+        # Read the CSV data
+        reader = csv.DictReader(csv_file)
+        ic(reader)
+        # Convert each row into the desired structure
+        for row in reader:
+            item = {
+                    'english': row['english'],
+                    'danish': row['danish'],
+                    'spanish': row['spanish']
+               
+            }
+            # Append the dictionary to the list
+            data[row['key']] = (item)
+ 
+        # Convert the data to JSON
+        json_data = json.dumps(data, ensure_ascii=False, indent=4)
+        # ic(data)
+ 
+        # Save data to the file
+        with open("dictionary.json", 'w', encoding='utf-8') as f:
+            f.write(json_data)
+ 
+        return "ok"
+    except Exception as ex:
+        ic(ex)
+        return str(ex)
+    finally:
+        pass
+
+
+
---
+++ x.py
@@ -0,0 +1,60 @@
+from flask import request, make_response, render_template
+import mysql.connector
+import re 
+import json
+import os
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from functools import wraps
+
+from icecream import ic
+ic.configureOutput(prefix=f'----- | ', includeContext=True)
+
+UPLOAD_ITEM_FOLDER = './images'
+
+def db():
+    try:
+        host = "sofievooo.mysql.pythonanywhere-services.com" if "PYTHONANYWHERE_DOMAIN" in os.environ else "mariadb"
+        database = "sofievooo$default" if "PYTHONANYWHERE_DOMAIN" in os.environ else "x"
+        user = "sofievo" if "PYTHONANYWHERE_DOMAIN" in os.environ else "root"
+        password = "MyPasswordForYou" if "PYTHONANYWHERE_DOMAIN" in os.environ else "password"
+
+        db = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        cursor = db.cursor(dictionary=True)
+        return db, cursor
+    except mysql.connector.Error as e:
+        print("DB connection error:", e, flush=True)
+        raise Exception("Twitter exception - Database under maintenance", 500)
+
+
+##############################
+def no_cache(view):
+    @wraps(view)
+    def no_cache_view(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
+    return no_cache_view
+
+
+##############################
+google_spread_sheet_key = "1SbJF1joPi3RQhuTERWlubJ67L98uAL-_ycwVouiP2zw"
+allowed_languages = ["english", "danish", "spanish"]
+default_language = "english"
+
+##############################
+def lans(key):
+    with open("dictionary.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data[key][default_language]
+
